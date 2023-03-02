e514e715b0af3651df05f6dbff086020dfeb8481
Aleksandar Galbulev <aleksandar.galbulev@iodigital.com>
2023-03-02 18:52:52 +0200

    More changes

--- Server/ContactManaging/ContactManaging/Data/Contact.cs
+++ Server/ContactManaging/ContactManaging/Data/Contact.cs
@@ -4,16 +4,16 @@
     {
         public int Id { get; set; }
 
-        public string FirstName { get; set; }
+        public string? FirstName { get; set; }
 
-        public string Surname { get; set; }
+        public string? Surname { get; set; }
 
-        public DateTime DateOfBirth { get; set; }
+        public DateTime? DateOfBirth { get; set; }
 
-        public string Address { get; set; }
+        public string? Address { get; set; }
 
-        public string PhoneNumber { get; set; }
+        public string? PhoneNumber { get; set; }
 
-        public string Iban { get; set; }
+        public string? Iban { get; set; }
     }
 }
--- Server/ContactManaging/ContactManaging/Data/ContactsDbContext.cs
+++ Server/ContactManaging/ContactManaging/Data/ContactsDbContext.cs
@@ -5,16 +5,32 @@ namespace ContactManaging.Data
     public class ContactsDbContext : DbContext
     {
         public ContactsDbContext(DbContextOptions<ContactsDbContext> options) : base(options)
-        { 
-        
+        {
+
         }
 
         public DbSet<Contact> Contacts { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Contact>(build => {
+            modelBuilder.Entity<Contact>(build =>
+            {
                 build.HasKey(p => p.Id);
+
+                build.Property(p => p.Id)
+                    .ValueGeneratedOnAdd();
+
+                build.Property(p => p.FirstName)
+                    .IsRequired();
+
+                build.Property(p => p.Surname)
+                    .IsRequired();
+
+                build.Property(p => p.DateOfBirth)
+                    .IsRequired();
+
+                build.Property(p => p.Address)
+                    .HasMaxLength(200);
             });
         }
     }
